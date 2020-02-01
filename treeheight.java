defa7793b1130efaa351565bb0021478e62f073c
DILVAR SINGH <dilvar789@gmail.com>
2020-02-01 20:21:45 +0530

    Find the height of a tree.

---
+++ treeheight.java
@@ -0,0 +1,86 @@
+class Node{
+	int data;
+	Node left;
+	Node right;
+	public Node(int d)
+	{
+		data=d;
+		left=null;
+		right=null;
+	}
+}
+
+public class treeheight 
+{
+static Node root;
+
+public treeheight()
+{
+root=null;
+}
+public treeheight(int k)
+{
+ root=new Node(k);
+}
+public void insertt(int d)
+{
+	root=insert(root,d);
+}
+public static Node insert(Node root,int d)
+{
+	if(root==null)
+	{
+		root=new Node(d);
+		return root;
+	}
+	else if(root.data>d)
+		{
+			root.left=insert(root.left,d);
+		
+		}
+		else if(root.data<d)
+		{
+			root.right=insert(root.right,d);
+		
+		}
+	return root;
+}
+
+
+public static int height(Node root)
+{
+	
+	if(root==null)
+	{
+		return 0;
+		
+	}
+	else
+	{
+		int lheight=height(root.left);
+		int rheight=height(root.right);
+		if(lheight>rheight)
+		{
+			return(lheight+1);
+		}
+		else
+		{
+			return(rheight+1);
+			
+		}
+	}
+}
+public static void main(String[] args) {
+
+treeheight bt=new treeheight();
+bt.insertt(1);
+bt.insertt(2);
+bt.insertt(3);
+bt.insertt(4);
+bt.insertt(5);
+
+int h=height(root)-1;
+System.out.println("height="+h);
+
+}
+}
